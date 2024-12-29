3c08bacd9121fbe0327872c02f638483841f3ea0
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 14:50:52 +0700

    menambahkan fungsi untuk mengelola status, menambahkan fungsi insertMhs untuk memasukkan data mahasiswa

--- app/src/main/java/com/example/praktikum12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum12/ui/viewmodel/InsertViewModel.kt
@@ -3,8 +3,25 @@ package com.example.praktikum12.ui.viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel(){
     var uiState by mutableStateOf(InsertUiState())
         private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
