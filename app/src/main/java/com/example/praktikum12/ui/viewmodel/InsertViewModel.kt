6a3836fc836c8015301bb460cb76fc4297e472de
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 14:59:19 +0700

    menambahkan fungsi InsertUiEvent menjadi entitas mahasiswa,menambahkan fungsi untuk mengonversi entitas mahasiswa menjadi status dan event

--- app/src/main/java/com/example/praktikum12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum12/ui/viewmodel/InsertViewModel.kt
@@ -2,8 +2,10 @@ package com.example.praktikum12.ui.viewmodel
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.praktikum12.model.Mahasiswa
 import com.example.praktikum12.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
@@ -37,4 +39,26 @@ data class InsertUiEvent(
     val jenisKelamin: String = "",
     val kelas: String = "",
     val angkatan: String = "",
+)
+
+fun InsertUiEvent.toMhs():Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs():InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
 )
