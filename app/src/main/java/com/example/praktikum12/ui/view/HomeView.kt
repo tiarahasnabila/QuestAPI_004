3dd6b1fcdf3d2233132c3d20958be0689212ac8c
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 16:08:16 +0700

    Menambahkan OnError untuk Tampilan Kesalahan dengan Opsi Retry

--- app/src/main/java/com/example/praktikum12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/praktikum12/ui/view/HomeView.kt
@@ -1,13 +1,16 @@
 package com.example.praktikum12.ui.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -118,4 +121,30 @@ fun OnLoading(modifier: Modifier = Modifier) {
         painter = painterResource(R.drawable.gambar),
         contentDescription = stringResource(R.string.loading)
     )
+}
+
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.gambarlagi),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(
+            onClick = retryAction
+        ) {
+            Text(stringResource(R.string.retry))
+        }
+    }
 }
