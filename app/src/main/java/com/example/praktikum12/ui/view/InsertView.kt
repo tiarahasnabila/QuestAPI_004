def47aeb163e06a62ddd45b2de2ada93fdf72572
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 15:30:46 +0700

    Menambahkan objek destinasiEntry untuk navigasi ke halaman mahasiswa

---
+++ app/src/main/java/com/example/praktikum12/ui/view/InsertView.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum12.ui.view
+
+import com.example.praktikum12.ui.navigation.DestinasiNavigasi
+
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
