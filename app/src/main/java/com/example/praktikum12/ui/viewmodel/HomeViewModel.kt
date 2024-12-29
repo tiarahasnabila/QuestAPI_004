1a8877d9d231ee43cc9afabf70f71bf2ad1422d7
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 15:25:58 +0700

    memperbarui file HomeViewModel dan menambahkan fungsi untuk mendapatkan dan menghapus mahasiswa

--- app/src/main/java/com/example/praktikum12/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum12/ui/viewmodel/HomeViewModel.kt
@@ -1,22 +1,56 @@
 package com.example.praktikum12.ui.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.praktikum12.model.Mahasiswa
 import com.example.praktikum12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import okio.IOException
 
-
-sealed class HomeUiState{
-    data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
-    object Error : HomeUiState()
-    object Loading : HomeUiState()
+sealed class HomeuiState{
+    data class Success(
+        val mahasiswa: List<Mahasiswa>
+    ): HomeuiState()
+    object Error: HomeuiState()
+    object Loading: HomeuiState()
 }
 
-class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel(){
-    var mhsUiState:HomeUiState by mutableStateOf(HomeUiState.Loading)
+class HomeViewModel(
+    private val mhs: MahasiswaRepository
+): ViewModel(){
+    var mhsUiState: HomeuiState by mutableStateOf(HomeuiState.Loading)
         private set
 
     init {
         getMhs()
+    }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUiState = HomeuiState.Loading
+            mhsUiState = try {
+                HomeuiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException){
+                HomeuiState.Error
+            } catch (e: HttpException){
+                HomeuiState.Error
+            }
+        }
+    }
+
+    fun deleteMhs(nim: String){
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException){
+                HomeuiState.Error
+            } catch (e: HttpException){
+                HomeuiState.Error
+            }
+        }
     }
 }
