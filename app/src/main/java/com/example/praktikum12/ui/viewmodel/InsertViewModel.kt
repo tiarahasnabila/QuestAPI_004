48ac2d1a1250e974390a5cfc6280208fe80d0c02
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 14:56:39 +0700

    menambahkan data class InsertUiEvent untuk mengelola input

--- app/src/main/java/com/example/praktikum12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum12/ui/viewmodel/InsertViewModel.kt
@@ -29,3 +29,12 @@ class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel(){
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
