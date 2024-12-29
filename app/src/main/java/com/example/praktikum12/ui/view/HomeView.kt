889d58b02e27b15359a8f42193d018387b59f492
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 15:49:07 +0700

    Menambahkan Object DestinasiHome untuk Navigasi Halaman Utama Mahasiswa

---
+++ app/src/main/java/com/example/praktikum12/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum12.ui.view
+
+import com.example.praktikum12.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
