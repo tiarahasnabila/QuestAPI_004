d550994b9f38a0fe1fbc415771a6651eeda5fdaa
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 16:49:01 +0700

    memperbarui file main activity

--- app/src/main/java/com/example/praktikum12/MainActivity.kt
+++ app/src/main/java/com/example/praktikum12/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum12.ui.theme.Praktikum12Theme
+import com.example.praktikum12.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Praktikum12Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
