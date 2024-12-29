81e5ae9d6d88b0a61966e90acacefcf944d6a2fe
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 15:03:59 +0700

    Menambahkan Class HomeViewModel dan mhsUiState

--- app/src/main/java/com/example/praktikum12/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum12/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.praktikum12.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
 import com.example.praktikum12.model.Mahasiswa
+import com.example.praktikum12.repository.MahasiswaRepository
 
 
 sealed class HomeUiState{
@@ -9,5 +12,11 @@ sealed class HomeUiState{
     object Loading : HomeUiState()
 }
 
-class HomeViewModel {
+class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUiState:HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
 }
