af943c66bef00d8b9491c068de2d1394b6c1501c
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-29 14:20:25 +0700

    Membuat Interface AppContainer untuk Dependency Injection

--- app/src/main/java/com/example/praktikum12/di/MahasiswaContainer.kt
+++ app/src/main/java/com/example/praktikum12/di/MahasiswaContainer.kt
@@ -1,2 +1,14 @@
 package com.example.praktikum12.di
 
+import com.example.praktikum12.repository.MahasiswaRepository
+import com.example.praktikum12.repository.NetworkKontakRepository
+import com.example.praktikum12.service_api.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer{
+    val kontakRepository: MahasiswaRepository
+}
+
