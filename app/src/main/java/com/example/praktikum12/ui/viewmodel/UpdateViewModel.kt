c3eb3b7c82363807bb1649d9d015e08089ea9f76
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-30 13:51:08 +0700

    menambahkan file UpdateViewModel untuk mengupdate dari data mahasiswa yang telah ditambahkan

---
+++ app/src/main/java/com/example/praktikum12/ui/viewmodel/UpdateViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.praktikum12.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum12.model.Mahasiswa
+import com.example.praktikum12.repository.MahasiswaRepository
+import com.example.praktikum12.ui.view.DestinasiUpdate
+import kotlinx.coroutines.launch
+
+class UpdateViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+): ViewModel(){
+    var updateUiState by mutableStateOf(InsertUiState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = mahasiswaRepository.getMahasiswabyNim(_nim)
+                .toUiStateMhs()
+        }
+    }
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        updateUiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun updateMhs(){
+        viewModelScope.launch {
+            try {
+                mahasiswaRepository.updateMahasiswa(_nim, updateUiState.insertUiEvent.toMhs())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
